ccedd38ee587a3498d9734735f993461bcbdcfd0
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-15 19:57:27 +0530

    Added wages for month

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,26 +1,27 @@
-
 public class EmployeeWages {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int fullTime = 1;
 		int partTime = 2;
 		int wagePerHour = 20;
-		int workingHour;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch(empCheck)
-		{
-			case 1:
-			System.out.println("Employee is present");
-			workingHour = 8;
-			break;
-			case 2:
-			System.out.println("part time employee is present");
-			workingHour = 4;
-			break;
-			default:
-			System.out.println("Employee is absent");
-			workingHour = 0;
+		int workingDays = 20;
+		int workingHour, empCheck , day, wagesPerday;
+		int totalWages = 0;
+		for(day=1;day<=workingDays;day++) {
+			empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch(empCheck) {
+				case 1:
+				workingHour = 8;
+				break;
+				case 2:
+				workingHour = 4;
+				break;
+				default:
+				workingHour = 0;
+			}
+			wagesPerday = workingHour * wagePerHour;
+			totalWages = totalWages + wagesPerday;
 		}
-		System.out.println("Employee Wage per day is " + wagePerHour * workingHour);
+		System.out.println("Employee Wage per  month " + totalWages);
 	}
 }
