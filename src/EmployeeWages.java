13899a50b1bade91a1117f63cbbc0fbb4156adf4
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-15 20:04:31 +0530

    Added wages for month for max hours

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -5,10 +5,13 @@ public class EmployeeWages {
 		int partTime = 2;
 		int wagePerHour = 20;
 		int workingDays = 20;
-		int workingHour, empCheck , day, wagesPerday;
-		int totalWages = 0;
-		for(day=1;day<=workingDays;day++) {
-			empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		int maxHoursInMonth = 100;
+		int workingHour, totalEmployeeWage;
+		int totalWorkingHour = 0;
+		int totalWorkingDays = 0;
+		while(totalWorkingHour <= maxHoursInMonth && totalWorkingDays < workingDays) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch(empCheck) {
 				case 1:
 				workingHour = 8;
@@ -19,9 +22,10 @@ public class EmployeeWages {
 				default:
 				workingHour = 0;
 			}
-			wagesPerday = workingHour * wagePerHour;
-			totalWages = totalWages + wagesPerday;
+			totalWorkingHour += workingHour;
+			System.out.println("Day : " + totalWorkingDays + " Emp Hrs : " +totalWorkingHour);
 		}
-		System.out.println("Employee Wage per  month " + totalWages);
+		totalEmployeeWage = totalWorkingHour * wagePerHour;
+		System.out.println("Total Employee wages " + totalEmployeeWage);
 	}
 }
